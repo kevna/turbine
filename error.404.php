bc9481b2a28438dfbbe148ef758162cb8eefc501
kevna <aam13@aber.ac.uk>
2013-08-09 23:48:13 +0100

    removed duplicate footer

--- error.404.php
+++ error.404.php
@@ -18,12 +18,6 @@
 	</p>
 	<a href="./">Return to Home</a>
 </div>
-<footer>
-	<span class="validation">
-		<a href="http://validator.w3.org/check?uri=http%3a%2f%2fusers%2eaber%2eac%2euk%2faam13%2fturbine%2ferror%2e404%2ehtml">HTML5</a>
-		<a href="http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fusers.aber.ac.uk%2Faam13%2Fturbine%2Ferror.404.html">CSS3</a>
-	</span>
-</footer>
 <?php include "footer.php" ?>
 </div>
 </body>
