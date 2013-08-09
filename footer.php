9a4398d7134aab57794b6df2b9054cff3d640b0c
kevna <aam13@aber.ac.uk>
2013-08-09 23:38:43 +0100

    added footer.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer>
+	<span class="validation">
+		<a href="http://validator.w3.org/check/referer">HTML5</a>
+		<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS3</a>
+	</span>
+	<!--<span class="center">other links</span> -->
+	<span class="right">
+<?php
+	echo "\t\t", $xml->baseplateName, ": \n";
+	echo "\t\t<span class=\"", $xml->state, "\">", $xml->state, "</span> \n";
+	echo "\t\t(", $xml->dateTime, ")\n";
+?>
+	</span>
+</footer>
