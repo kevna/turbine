cccbc28f2176e2e5475ae85770b34f182f515204
kevna <aam13@aber.ac.uk>
2013-08-22 23:11:33 +0100

    moved to root references for css

--- resources/include/head.php
+++ resources/include/head.php
@@ -2,7 +2,7 @@
 	<meta name="HandheldFriendly" content="True" />
 	<meta name="Viewport" content="width=device-width" />
 	<link rel="icon" sizes="256x256" href="favicon.ico" />
-	<link rel="stylesheet" href="main.css" />
+	<link rel="stylesheet" href="/turbine/main.css" />
 <?php
 	$xml = simplexml_load_file("current.xml");
 	$syncTime = date('d/m/Y H:i', strtotime($xml->dateTime));
