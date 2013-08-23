1d9c41a43c02c106479a8df7cf47219656b72225
kevna <aam13@aber.ac.uk>
2013-08-23 22:14:23 +0100

    made nav float on page

--- resources/include/head.php
+++ resources/include/head.php
@@ -3,6 +3,20 @@
 	<meta name="Viewport" content="width=device-width" />
 	<link rel="icon" sizes="256x256" href="favicon.ico" />
 	<link rel="stylesheet" href="/turbine/main.css" />
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script>
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > parseFloat($("header").css("height"))) {
+				$("nav").addClass("scrolling");
+			} else {
+				$("nav").removeClass("scrolling");
+			}
+		});
+		$(window).resize(function() {
+			$("#content").css("padding-bottom", $("footer").css("height"));
+		});
+	</script>
 <?php
 	$xml = simplexml_load_file("current.xml");
 	$syncTime = date('d/m/Y H:i', strtotime($xml->dateTime));
