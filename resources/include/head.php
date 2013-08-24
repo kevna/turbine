3f812a02895c433eee70665b57b0c53d6a2fe578
kevna <aam13@aber.ac.uk>
2013-08-24 14:26:53 +0100

    added MIME types and fixed scrolling nav bug

--- resources/include/head.php
+++ resources/include/head.php
@@ -1,15 +1,17 @@
-	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-	<meta name="HandheldFriendly" content="True" />
-	<meta name="Viewport" content="width=device-width" />
+	<meta http-equiv="Content-Type"	content="text/html;charset=utf-8" />
+	<meta name="HandheldFriendly"	content="True" />
+	<meta name="Viewport"		content="width=device-width" />
 	<link rel="icon" sizes="256x256" href="favicon.ico" />
-	<link rel="stylesheet" href="/turbine/main.css" />
-	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-	<script>
+	<link rel="stylesheet"	type="text/css"	href="/turbine/main.css" />
+	<script type="text/javascript"	src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<script type="text/javascript"	src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript">
 		$(window).scroll(function() {
 			if ($(this).scrollTop() > parseFloat($("header").css("height"))) {
+				$("#content").css("margin-top", $("nav").css("height"));
 				$("nav").addClass("scrolling");
 			} else {
+				$("#content").css("margin-top", 7);
 				$("nav").removeClass("scrolling");
 			}
 		});
