3a17a9870a45d5a0fa9cbc552da3ef7f5589d605
kevna <aam13@aber.ac.uk>
2013-08-27 20:53:22 +0100

    assorted updates to nav form

--- resources/include/nav.php
+++ resources/include/nav.php
@@ -3,18 +3,21 @@
 	<a id="nav-404"		href="error.404.php"	>Error 404</a>
 	<a id="nav-403"		href="error.hal.php"	>Error 403</a>
 	<form id="nav-history" action="javascript:alert('submission failed')">
-		<span>From:</span><span class="alert" id="form-feedback"></span>
+		<span>From:</span><span class="alert" id="from-feedback"></span>
 		<input name="from-date"	type="date"	size="8"	title="starting date" />
 		<input name="from-time"	type="time"	size="4"	title="starting time" />
 		<span>To:</span>
 		<input name="to-date"	type="date"	size="8"	title="finishing date" />
 		<input name="to-time"	type="time"	size="4"	title="finishing time" />
-		<input value="Go"	type="submit" />
+		<input value="Go"	type="submit" /><span class="alert" id="form-feedback"></span>
 		<!--<a id="nav-submit">Submit</a>-->
 	</form>
 	<script>
 		$("input").focus(function() {
 			$(this).addClass("focused");
+		});
+		$("input").change(function() {
+			$(this).removeClass("incorrect");
 		});
 		$("input").blur(function() {
 			$(this).removeClass("focused");
@@ -25,11 +28,23 @@
 		$("#nav-history").submit(function(){
 			var from = "";
 			var to = "";
-			from = $("input[name=from-date]").val();
+			if ($("input[name=from-date]").val() != "") {
+				if (isNaN(Date.parse($("input[name=from-date]").val()))) {
+					$("input[name=from-date]").addClass("incorrect");
+					$("#form-feedback").text("Invalid!").show().fadeOut(1000);
+					return false;
+				}
+				from = $("input[name=from-date]").val();
+			} else {
+				$("#from-feedback").text("Required!").show().fadeOut(1000);
+			}
 			if ($("input[name=from-time]").val() != "") {
 				from += "T" + $("input[name=from-time]").val();
 			}
 			if ($("input[name=to-date]").val() != "") {
+				if (isNaN(Date.parse($("input[name=to-date]").val()))) {
+					$("input[name=to-date]").addClass("incorrect");
+				}
 				to = $("input[name=to-date]").val();
 			}
 			if ($("input[name=to-time]").val() != "") {
@@ -37,8 +52,6 @@
 			}
 			if (from != "") {
 				window.location.href = "../resting/trial/" + from + "/" + to;
-			} else {
-				$("#form-feedback").text("Required!").show().fadeOut(1000);
 			}
 			return false;
 		});
