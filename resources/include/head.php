1d68199771e8fb8c92a0cdbd155fb26446419e4c
kevna <aam13@aber.ac.uk>
2013-08-29 00:37:13 +0100

    added assorted metadata

--- resources/include/head.php
+++ resources/include/head.php
@@ -1,8 +1,12 @@
 	<!--<meta http-equiv="Content-Type"	content="text/html;charset=utf-8" />-->
 	<meta charset="utf-8" />
+	<meta name="author"		content="Aaron Moore" />
+	<meta name="description"	content="Online interface for QuietRevolution wind turbines" />
+	<meta name="doc-class"		content="development/testing" />
+	<meta name="generator"		content="vim/my-brain" />
 	<meta name="HandheldFriendly"	content="True" />
-	<meta name="Viewport"		content="width=device-width" />
-	<link rel="icon" sizes="256x256" href="favicon.ico" />
+	<meta name="Viewport"		content="width=device-width; maximum-scale=3.4" />
+	<link rel="icon"	sizes="256x256"	href="favicon.ico" />
 	<link rel="stylesheet"	type="text/css"	href="/turbine/main.css" />
 	<script type="text/javascript"	src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 	<script type="text/javascript"	src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
